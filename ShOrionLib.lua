85412cad386e83ba29ab7b65bacf9dcb54a39123
xiessarkia <spoilerandbutter@gmail.com>
2024-10-13 23:40:15 +0800

    ShOrionLib.lua

--- ShOrionLib.lua
+++ ShOrionLib.lua
@@ -16,10 +16,10 @@ local OrionLib = {
         Default = {
                 Main = Color3.fromRGB(212, 32, 32),      -- Darker reddish
                 Second = Color3.fromRGB(194, 22, 22),      -- Slightly darker red
-                Stroke = Color3.fromRGB(143, 0, 0),      -- Darker stroke
+                Stroke = Color3.fromRGB(124, 0, 0),      -- Darker stroke
                 Divider = Color3.fromRGB(235, 77, 77),     -- Divider with lower intensity
                 Text = Color3.fromRGB(248, 235, 235),        -- Keep text light for contrast
-                TextDark = Color3.fromRGB(124, 16, 16)     -- Adjusted to valid RGB range
+                TextDark = Color3.fromRGB(94, 20, 20)     -- Adjusted to valid RGB range
             }
     },
     SelectedTheme = "Default",
